1c8c2e14db9e91b803496e82f02fd0fe8c266eaa
greg@gregPredator <greg19850@gmail.com>
2022-07-09 20:10:20 +0100

    created showMessage functon, for password input

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const passwords = ['usER', 'paSswOrd', 'AnytHing'];
 const messages = ['test message', 'working', 'error'];
 
 
-const showMessage = () => { }
+const showMessage = (event) => {
+    let text = event.target.value.toLowerCase();
+    div.textContent = ''
+
+    passwords.forEach((password, i) => {
+        if (text === password.toLowerCase()) {
+            div.textContent = messages[i]
+        }
+    });
+};
 
 input.addEventListener('input', showMessage)
