9c624119d5b34222ccf46340534432a49c3674b8
greg@gregPredator <greg19850@gmail.com>
2022-07-09 19:51:32 +0100

    created variables and eventListener in script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('input');
+const div = document.querySelector('div');
+
+const passwords = ['usER', 'paSswOrd', 'AnytHing'];
+const messages = ['test message', 'working', 'error'];
+
+
+const showMessage = () => { }
+
+input.addEventListener('input', showMessage)
